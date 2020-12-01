cbc183e438e6f0ed55ec0d44d1ac8c1749c6f038
Mr-Hashtag <73663791+Mr-Hashtag@users.noreply.github.com>
2020-12-01 23:38:55 +0330

    creating a new folder for constants and using consts for action types

--- src/action/index.js
+++ src/action/index.js
@@ -2,40 +2,42 @@ import axios from 'axios';
 
 import {jsonServer} from '../apis/jsonServer';
 import history from '../history';
+import {REG_POST,
+    FETCH_ACC,
+    FETCH_PRODUCTS,
+    FETCH_PRODUCT,
+    SIGN_IN,
+    SIGN_OUT
+} from '../constants/types';
 
 export const registeration=(formValues)=>async dispatch=>{
     const response= await jsonServer.post("/accounts",formValues)
-    dispatch({type:"REG_POST", payload:response.data})
+    dispatch({type:REG_POST, payload:response.data})
     history.push("/auth")
 }
 
 export const fetchAccounts=()=>async dispatch=>{
     const response=await jsonServer.get("/accounts")
-    dispatch({type:"FETCH_ACC", payload:response.data})
+    dispatch({type:FETCH_ACC, payload:response.data})
 }
-
-// export const postProducts=()=>async dispatch=>{
-//     const response=await jsonServer.post("/products")
-//     dispatch({type:"POST_PRODUCTS", payload:response.data})
-// }
 
 export const fetchProducts=()=>async dispatch=>{
     const response= await axios.get('https://fakestoreapi.com/products')
-    dispatch({type:"FETCH_PRODUCTS", payload:response.data})
+    dispatch({type:FETCH_PRODUCTS, payload:response.data})
 }
 
 export const fetchProduct=(id)=>async dispatch=>{
     const response=await axios.get(`https://fakestoreapi.com/products/${id}`)
-    dispatch({type:"FETCH_PRODUCT", payload:response.data})
+    dispatch({type:FETCH_PRODUCT, payload:response.data})
 }
 
 export const signIn=(name)=>{
     localStorage.setItem("isSignedIn",true)
     localStorage.setItem("name",name)
-    return {type:"SIGN_IN", payload:name}
+    return {type:SIGN_IN, payload:name}
 }
 
 export const signOut=()=>{
     localStorage.clear();
-    return {type:"SIGN_OUT"}
+    return {type:SIGN_OUT}
 }
--- src/reducer/accounts.js
+++ src/reducer/accounts.js
@@ -1,7 +1,10 @@
+import {
+    FETCH_ACC
+} from '../constants/types';
 
 const accounts=(state=null,action)=>{
     switch(action.type){
-        case "FETCH_ACC":
+        case FETCH_ACC:
             return {...state,[action.payload.id]:action.payload}
         default:
             return {...state}
